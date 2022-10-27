1a16780c5045c1fc100fbd3c8cea67593b3c4205
N1kOk <n1k_ok@bk.ru>
2022-10-27 17:49:19 +0500

    parma

--- src/views/OfficeView.vue
+++ src/views/OfficeView.vue
@@ -3,8 +3,11 @@
 		<div class="min-h-[inherit] flex flex-col justify-between">
 			<div class="flex h-[75px]">
 				<div class="flex space-x-16">
-					<RouterLink to="/user"><img class="h-full" src="/images/user.svg" alt="user"></RouterLink>
-					<RouterLink to="/calendar"><img class="h-full" src="/images/calendar.svg" alt="calendar">
+					<RouterLink to="/user">
+						<img class="h-full" src="/images/user.svg" alt="user">
+					</RouterLink>
+					<RouterLink to="/calendar">
+						<img class="h-full" src="/images/calendar.svg" alt="calendar">
 					</RouterLink>
 				</div>
 				<div class="px-10 py-4 ml-auto flex border border-2 rounded-full space-x-8"
@@ -16,7 +19,7 @@
 				</div>
 			</div>
 
-			<div class="flex justify-between">
+			<div class="flex justify-between items-end">
 				<div class="w-[75px] p-4 flex flex-col space-y-4 text-center bg-gray-50">
 					<div class="font-bold">Этаж</div>
 					<div class="cursor-pointer select-none" v-for="i in 4">
@@ -24,7 +27,13 @@
 					</div>
 				</div>
 
-				<div class="flex flex-wrap bg-gray-50"></div>
+				<div class="w-[70%] px-16 py-8 bg-gray-50">
+					<div class="w-[550px] mx-auto flex flex-wrap gap-[50px]">
+						<div class="w-[150px] h-[150px] flex flex-wrap bg-white" v-for="i in 5">
+							<div class="w-[20%] h-[20%] m-[15%] bg-black" v-for="i in 4"></div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
