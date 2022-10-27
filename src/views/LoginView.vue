dd7200c916f69faa7b028eb93292574eed8b6c28
N1kOk <n1k_ok@bk.ru>
2022-10-27 17:02:53 +0500

    parma

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,8 +2,8 @@
 	<div class="w-[500px] mx-auto p-8 bg-black-500">
 		<div class="w-[300px] mx-auto space-y-12">
 			<div class="w-full flex flex-col space-y-4">
-				<input type="text" placeholder="Логин">
-				<input type="password" placeholder="Пароль">
+				<input class="login-form" type="text" placeholder="Логин">
+				<input class="login-form" type="password" placeholder="Пароль">
 				<RouterLink class="text-white text-xl underline underline-offset-4" to="/restore">Забыли пароль?</RouterLink>
 			</div>
 			<div class="w-full flex flex-col space-y-4 text-white text-2xl font-bold">
--- src/views/OfficeView.vue
+++ src/views/OfficeView.vue
@@ -1,8 +1,31 @@
 <template>
 	<div class="w-full min-h-[calc(100vh-160px)] text-left space-y-8">
-		<div class="text-3xl font-bold">Офис в каком городе Вас интересует?</div>
-		<div class="flex justify-between">
+		<div class="min-h-[inherit] flex flex-col justify-between">
+			<div class="flex h-[75px]">
+				<div class="flex space-x-16">
+					<RouterLink to="/user"><img class="h-full" src="/images/user.svg" alt="user"></RouterLink>
+					<RouterLink to="/calendar"><img class="h-full" src="/images/calendar.svg" alt="calendar">
+					</RouterLink>
+				</div>
+				<div class="px-10 py-4 ml-auto flex border border-2 rounded-full space-x-8"
+				     onclick="document.getElementById('search').focus()">
 
+					<img class="pointer-events-none" src="/images/search.svg" alt="search">
+					<input class="w-[250px] h-max my-auto text-2xl font-bold placeholder-gray-50 outline-none"
+					       id="search" type="text" placeholder="Поиск сотрудника" autocomplete="off">
+				</div>
+			</div>
+
+			<div class="flex justify-between">
+				<div class="w-[75px] p-4 flex flex-col space-y-4 text-center bg-gray-50">
+					<div class="font-bold">Этаж</div>
+					<div class="cursor-pointer select-none" v-for="i in 4">
+						<img class="w-full pointer-events-none" :src="`/images/floors/${i}.svg`" alt="floor">
+					</div>
+				</div>
+
+				<div class="flex flex-wrap bg-gray-50"></div>
+			</div>
 		</div>
 	</div>
 </template>
@@ -11,7 +34,7 @@
 import { useRoute } from 'vue-router'
 import { setCurrentOffice } from '@/scripts/offices'
 
-const officeId = useRoute().params.officeId as string
+const officeId = +useRoute().params.officeId
 
-setCurrentOffice(+officeId)
+setCurrentOffice(officeId)
 </script>
