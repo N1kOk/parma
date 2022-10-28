b747c00a2983bad83f277dd387d4e7d1d970b5de
N1kOk <n1k_ok@bk.ru>
2022-10-28 22:23:49 +0500

    parma

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -4,18 +4,20 @@
 			<div class="text-4xl font-bold">Мой профиль</div>
 			<div class="w-full h-[calc(100%-112px)] p-8 bg-black-500">
 				<div class="h-full flex flex-col justify-between">
-					<div class="flex space-x-12">
+					<div class="flex space-x-16">
 						<img class="h-[192px]" src="/images/man.png" alt="office">
 						<div class="w-[225px] space-y-2">
-							<input class="w-full px-4 text-2xl rounded-full" type="text">
-							<input class="w-full px-4 text-2xl rounded-full" type="text">
-							<input class="w-full px-4 text-2xl rounded-full" type="text">
-							<input class="w-full px-4 text-2xl rounded-full" type="text">
-							<input class="w-full px-4 text-2xl rounded-full" type="text">
+							<div class="relative" v-for="item in fields">
+								<input class="w-full px-4 text-2xl rounded-full" type="text" v-model="item.text" :disabled="!item.isEdited">
+								<img class="absolute -right-[75px] top-0 w-8 cursor-pointer" src="/images/pencil.svg" alt="pencil"
+								     v-show="isEdited" @click="item.isEdited = !item.isEdited">
+							</div>
 						</div>
 					</div>
 
-					<div class="w-max px-4 text-2xl rounded-xl bg-red cursor-pointer">Редактировать данные</div>
+					<div class="w-max px-4 text-2xl rounded-xl bg-red cursor-pointer" @click="isEdited = !isEdited">
+						Редактировать данные
+					</div>
 				</div>
 			</div>
 		</div>
@@ -30,5 +32,14 @@
 </template>
 
 <script setup lang="ts">
-import { offices } from '@/scripts/offices'
+import { ref } from 'vue'
+
+const isEdited = ref(false)
+const fields = ref([
+	{ text: '', isEdited: false },
+	{ text: '', isEdited: false },
+	{ text: '', isEdited: false },
+	{ text: '', isEdited: false },
+	{ text: '', isEdited: false },
+])
 </script>
