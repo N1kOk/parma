f423b56cc5c3f4b4c106e147721cbb4f3ea78e8a
N1kOk <n1k_ok@bk.ru>
2022-11-10 18:35:00 +0500

    parma

--- src/scripts/users.ts
+++ src/scripts/users.ts
@@ -1,9 +1,9 @@
 export type User =
-	{ [X in 'firstName' | 'lastName' | 'patronymic' | 'phone' | 'mail' | 'imageUrl']: string }
-	& { id: number, isAdmin: boolean }
+	{ [X in 'firstName' | 'lastName' | 'patronymic' | 'phone' | 'mail' | 'imageUrl']: string } &
+	{ id: number, isAdmin: boolean }
 
 
-export const users: { [X: string]: User } = {
+export let users: { [X: string]: User } = {
 	'test:test': {
 		id: Math.random(),
 		isAdmin: true,
@@ -27,6 +27,33 @@ export const users: { [X: string]: User } = {
 	},
 }
 
+initUsers()
+
 export function getUser(login: string, pass: string) {
 	return users[login + ':' + pass]
+}
+
+export function createUser(user: Omit<User, 'id' | 'imageUrl' | 'isAdmin'> & { pass: string }) {
+	for (const key in user)
+		if (!user[key as keyof typeof user])
+			throw `${key} = undefined`
+	
+	users[user.mail + ':' + user.pass] = {
+		id: Math.random(),
+		isAdmin: false,
+		imageUrl: '',
+		...user,
+	}
+	
+	saveUsers()
+}
+
+function initUsers() {
+	if (!localStorage.users) return
+	
+	users = JSON.parse(localStorage.users)
+}
+
+function saveUsers() {
+	localStorage.users = JSON.stringify(users)
 }
